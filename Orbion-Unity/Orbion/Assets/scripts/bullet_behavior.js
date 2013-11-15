7b06e9f299c6ca4b7b7220526b1bc853fa0672d2
DannyPaguiao <dpaghari@ucsc.edu>
2013-11-15 14:35:09 -0800

    Added Unity Project

---
+++ Orbion-Unity/Orbion/Assets/scripts/bullet_behavior.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+
+var bullet_Spd : float = 5.0;
+
+
+function Start () {
+
+}
+
+function Update () 
+
+{
+    rotateCharacter();
+}
+
+ 
+
+function rotateCharacter()
+
+{
+
+    var draai:float = Mathf.Atan2(  (Input.mousePosition.y - 0.5 * Screen.height) , 
+
+                                                (Input.mousePosition.x - 0.5 * Screen.width));
+	
+    draai *= Mathf.Rad2Deg;
+    var diry = Mathf.Sin(draai);
+    var dirx = Mathf.Cos(draai);
+    
+    transform.position.x += bullet_Spd * dirx;
+    transform.position.y += bullet_Spd * diry;
+    Debug.Log(draai);
+    transform.rotation.eulerAngles.z = draai + 90;
+    //transform.position. += bullet_Spd * Time.deltaTime;
+
+}
---
+++ Orbion-Unity/Orbion/Assets/scripts/player_controls.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var speed : float = 5.0;
+var bullet_prefab : Transform;
+
+
+function Update () {
+
+	if(Input.GetKey("w"))
+	transform.position.y += speed * Time.deltaTime;
+	
+	if(Input.GetKey("a"))
+	transform.position.x -= speed * Time.deltaTime;
+	
+	if(Input.GetKey("s"))
+	transform.position.y -= speed * Time.deltaTime;
+	
+	if(Input.GetKey("d"))
+	transform.position.x += speed * Time.deltaTime;
+
+	if(Input.GetMouseButtonDown(0))
+	Instantiate(bullet_prefab, transform.position, Quaternion.identity);
+
+}
